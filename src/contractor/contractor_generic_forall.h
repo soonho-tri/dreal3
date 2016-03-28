f6511f6f541b20b7b38f61860c91389494282f97
Soonho Kong <soonhok@cs.cmu.edu>
2016-03-28 17:29:44 -0400

    feat(contractor): extend prune to take clause_manager

--- src/contractor/contractor_generic_forall.h
+++ src/contractor/contractor_generic_forall.h
@@ -28,10 +28,13 @@ along with dReal. If not, see <http://www.gnu.org/licenses/>.
 #include <utility>
 #include <vector>
 #include "./config.h"
+#include "constraint/constraint.h"
+#include "contractor/contractor.h"
+#include "contractor/contractor_common.h"
+#include "icp/clause_manager.h"
 #include "opensmt/egraph/Enode.h"
 #include "opensmt/smtsolvers/SMTConfig.h"
 #include "util/box.h"
-#include "contractor/contractor.h"
 
 namespace dreal {
 class contractor_generic_forall : public contractor_cell {
